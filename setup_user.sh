56ebe6808d1ca4ddffed8d4ed2a67f8e81d20ba4
mdzaif <mdizaif@gmail.com>
2024-04-14 21:32:45 +0600

    Last Update: April 15, 2024

--- setup_user.sh
+++ setup_user.sh
@@ -1,10 +1,14 @@
 #!bin/bash
 #command: curl -s https://raw.githubusercontent.com/mdzaif/My_Setup/main/setup_user.sh | bash
+
+#admin account
+admin=$(whoami)
+
 # Add user 
-sudo useradd -g admin -s /bin/bash -c "Md. Zaif Imam Mahi" -m -d /home/zaifmahi zaifmahi
+sudo useradd -g $admin -s /bin/bash -c "Md. Zaif Imam Mahi" -m -d /home/zaifmahi zaifmahi
 
 #Set password
-passwd zaifmahi
+sudo passwd zaifmahi
 
 #Set user to the sudo group
 sudo usermod -aG sudo zaifmahi

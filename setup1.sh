f6b87887c764c8363abcb55c9c95a23fd48911cc
mdzaif <mdizaif@gmail.com>
2024-04-16 14:41:17 +0600

    Last Update: April 16, 2024

--- setup1.sh
+++ setup1.sh
@@ -9,15 +9,17 @@ usr=$(whoami)
 #configure bashrc
 curl -s https://raw.githubusercontent.com/mdzaif/My_Setup/main/bashcon.txt >> /home/$usr/.bashrc
 
-#configure vimrc
-#touch /home/$usr/.vimrc
-#touch ~/.vimrc.plug
-#mkdir ~/vimplug-plugins
+curl -fLo /home/$usr/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-#curl -s https://raw.githubusercontent.com/mdzaif/My_Setup/main/vim_plug.txt > /home/$usr/.vimrc.plug
 curl -s https://raw.githubusercontent.com/mdzaif/My_Setup/main/vimcon.txt > /home/$usr/.vimrc
 
 #read the new configure file
 source /home/$usr/.bashrc
-source /home/$usr/.vimrc
 
+
+echo "Configure Instraction"
+echo "---------------------"
+echo "Type$ source .vimrc"
+echo "open vim"
+echo "Tpye :PlugInstall"
+echo "source .vimrc"

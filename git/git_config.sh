04d9a930dcc995ba1d1a16def633127186766488
mdzaif <mdizaif@gmail.com>
2024-07-01 11:48:37 +0600

    config git in fresh environment

---
+++ git/git_config.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# For bootstrap setup
+
+# install the git
+which git > /dev/null
+
+if [ $? -ne 0 ]; then
+  sudo apt install git # for debian based
+else
+  echo "Installed"
+fi
+# config my environment
+
+read -p "User name: " usr
+read -p "Email:  " email
+git config --global user.name "$usr" && git config --global user.email "$email"
+git config --list
+
+# ssh key generation
+#curl -s https://raw.githubusercontent.com/mdzaif/My_Setup/main/git/sshkey.sh | bash

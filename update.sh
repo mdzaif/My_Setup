0d875ecc916c7e08d4ef637cdfb971b8203336b5
mdzaif <mdizaif@gmail.com>
2024-08-25 13:53:02 +0600

    [UPDATE]: make it more useful

--- update.sh
+++ update.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
 # description: Update package for ubuntu
 
-# update dpkg
-sudo apt update && sudo apt upgrade
+# update debian package(dpkg)
+echo "Check dpkg...."
+sleep 5
+clear
+sudo apt update 
+sudo apt upgrade -y
+echo "Exit status: $?"
+sleep  5
+clear
 
-# flatpak udpate
-flatpak update
+#flatpak update
+if [ $(flatpak --version > /dev/null && echo $?) -eq 0 ];then
+  echo "Check flatpak packages..."
+  sleep 5
+  clear
+  flatpak update
+  echo "Exit status: $?"
+  sleep 5
+  clear
+fi
 
 # snap update
+echo "Check snap packages..."
+sleep 5
+clear
 snap refresh
+echo "Exit status: $?"

7807b757473cf95bd3da5b0957fa5eabe50babbc
mdzaif <mdizaif@gmail.com>
2024-09-01 18:16:48 +0600

    [FIX]: resolve the file copy issue

--- install_myfont.sh
+++ install_myfont.sh
@@ -6,10 +6,10 @@ echo "Download the fonts..."
 wget -P ~/fonts/ https://raw.githubusercontent.com/mdzaif/My_Setup/main/manual/solution/font_back.zip
 
 sleep 2
-echo "File download complete" 
+echo "File download complete"
 echo "Extract the files..."
 sleep 2
-unzip ~/fonts/font_back.zip 
+unzip ~/fonts/font_back.zip -d ~/fonts/
 echo "Extract complete"
 sleep 2
 
@@ -19,16 +19,23 @@ echo "Start Setup..."
 sleep 2
 
 # Create TTF folder
-sudo mkdir /usr/share/fonts/TTF
+if [[ -d /usr/share/fonts/TTF ]];then
+  echo "Directory /usr/share/fonts/TTF exists"
+else
+  echo "Directory /usr/share/fonts/TTF not exists"
+  echo "Directory creating...."
+  sudo mkdir /usr/share/fonts/TTF
+  echo "Directory created at /usr/share/fonts/TTF"
+fi
 
 # Copy the fonts directory
-sudo cp ~/fonts/font_back/* /usr/share/fonts/TTF/
+sudo cp ~/fonts/fonts_back/* /usr/share/fonts/TTF/
 
 
 # Save the changes
 fc-cache -f -v
 
 echo "--------------------"
-echo "remove unnecessary files..."
+#echo "remove unnecessary files..."
 rm -rf ~/fonts/
 echo "Your done!"
